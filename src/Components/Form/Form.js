2ab57e0b6199c7fa4c42d0b38582f4b926242d97
Rishikesh <contactrishikeshhere@gmail.com>
2020-08-01 23:11:32 +0530

    added conditional rendering

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -7,8 +7,10 @@ class Form extends Component {
             teamName: "",
             whatsappNumber: "",
             required : true,
+            condition : false,
         }
-        this.handleChange = this.handleChange.bind(this)
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
     
     handleChange(event) {
@@ -17,30 +19,50 @@ class Form extends Component {
             [name]: value
         })
     }
+
+    handleSubmit(event) {
+        event.preventDefault()
+        this.setState({
+            condition : true,
+        })
+    }
     
     render() {
-        return (
-            <form> 
-                <input 
-                    type="text" 
-                    value={this.state.teamName} 
-                    name="teamName" 
-                    placeholder="First Name"
-                    required={this.state.required} 
-                    onChange={this.handleChange} 
-                />
-                <br />
-                <input 
-                    type="text" 
-                    value={this.state.whatsappNumber} 
-                    name="whatsappNumber" 
-                    placeholder="Last Name"
-                    required={this.state.required} 
-                    onChange={this.handleChange} 
-                />
-                <button>Submit</button>
-            </form>
-        )
+        // const cond = this.
+        // console.log("before ", this.state.condition)
+        if(this.state.condition){
+            return(
+                <div className="trueCondition">
+                    
+                </div>
+            )
+        }else{
+            return(
+                <form onSubmit={this.handleSubmit} className="form"> 
+                    <label>"Team Name"</label>
+                    <input 
+                        type="text" 
+                        value={this.state.teamName} 
+                        name="teamName" 
+                        placeholder="India"
+                        required={this.state.required} 
+                        onChange={this.handleChange} 
+                    />
+                    <br />
+                    <label>whatsappNumber</label>
+                    <input 
+                        type="text" 
+                        value={this.state.whatsappNumber} 
+                        name="whatsappNumber" 
+                        placeholder="eg +91 9876543210"
+                        required={this.state.required} 
+                        onChange={this.handleChange} 
+                    />
+                    <button typr="submit">Submit</button>
+                    {/* {console.log(this.state.condition)} */}
+                </form>
+            )
+        }
     }
 }
 

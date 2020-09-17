5f07d513115e38391adedccdcb03207554b82f0c
Rishikesh <contactrishikeshhere@gmail.com>
2020-09-17 23:31:32 +0530

    fixed the ui

--- src/App.js
+++ src/App.js
@@ -6,18 +6,18 @@ function App() {
   return (
     <div className="App">
 
-      <div className="video">
+      {/* <div className="video">
         <video id="videopart" preload="auto" autoPlay="true" loop="loop" muted="muted">
           <source src="cricpro.mp4" type="video/mp4"></source>
         </video>
-      </div>
+      </div> */}
 
-      <div className="other">
+      {/* <div className="other"> */}
         <Banner />
         <Developer /> 
         <Steps />
         <Form />
-      </div>
+      {/* </div> */}
         
 
     </div>
--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -4,7 +4,7 @@ import './Banner.css'
 const Banner = () => {
     return(
         <div className="mainBanner">
-            {/* <p> <span role="img">✨✨</span> IPL 2020<span role="img"> 🎉🎉 </span></p> */}
+            <p> <span role="img" aria-label="bat-ball">&#x1F389;&#x1F3CF;&#x1F3C6;&#x1F3CF;</span> IPL 2020<span role="img" aria-label="trophy"> &#x1F3CF;&#x1F3C6;&#x1F3CF;&#x1F389; </span></p>
         </div>
     )
 
